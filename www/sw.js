c539605c8063d7d21f66c177804e32129f82d36b
poohia <poohia.studio@gmail.com>
2021-10-08 09:56:25 +0200

    2. Soluce

---
+++ www/sw.js
@@ -0,0 +1,63 @@
+/** VARS */
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "offline.html";
+
+/** FUNCTIONS */
+
+/** Fetch */
+
+const respondWithFetchPromiseNavigate = (event) =>
+  new Promise((resolve) => {
+    event.preloadResponse.then(() => {
+      event.preloadResponse.then((preloadResponse) => {
+        if (preloadResponse) {
+          resolve(preloadResponse);
+        }
+
+        // Always try the network first.
+        fetch(event.request)
+          .then((networkResponse) => {
+            resolve(networkResponse);
+          })
+          // send cache offline.html
+          .catch(() => {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.match(OFFLINE_URL).then((cachedResponse) => {
+                resolve(cachedResponse);
+              });
+            });
+          });
+      });
+    });
+  });
+
+const fetchSW = (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(respondWithFetchPromiseNavigate(event));
+  }
+};
+
+/*********************************** */
+
+/** Install */
+const waitUntilInstallationPromise = () =>
+  new Promise((resolve) => {
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.add(new Request(OFFLINE_URL, { cache: "reload" })).then(resolve);
+    });
+  });
+
+const installSW = (event) => {
+  event.waitUntil(waitUntilInstallationPromise());
+
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
+};
+/*********************************** */
+
+/** INIT */
+self.addEventListener("install", installSW);
+
+self.addEventListener("fetch", fetchSW);

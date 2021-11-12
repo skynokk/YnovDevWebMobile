4cc797c2cccf0f8c2702481b80a541632a4a5be4
Jordan Azoulay <azoulay.jordan@gmail.com>
2021-11-12 12:09:55 +0100

    update mangas.tsx

--- mangas.tsx
+++ mangas.tsx
@@ -3,35 +3,35 @@ const mangas = [
     name: "One piece",
     description:
       "One Piece (ワンピース, Wan Pīsu?) est une série de mangas shōnen créée par Eiichirō Oda. Elle est prépubliée depuis le 22 juillet 1997 dans le magazine hebdomadaire Weekly Shōnen Jump.... ",
-    img: "img/mangas/onepiece.jpg",
+    img: "https://photos.tf1.fr/700/933/vignette-portrait-one-piece-4539bb-8fd616-0@1x.jpg",
     link: "https://fr.wikipedia.org/wiki/One_Piece",
   },
   {
     name: "Hunter × Hunter",
     description:
       "Hunter × Hunter est un shōnen manga écrit et dessiné par Yoshihiro Togashi. Il est pré-publié depuis mars 1998 dans l'hebdomadaire Weekly Shōnen Jump de l'éditeur Shūeisha, et a été compilé en trente-six tomes au 4 octobre 2018...",
-    img: "img/mangas/hunterhunter.jpg",
+    img: "https://fr.web.img5.acsta.net/pictures/19/08/01/09/52/4803203.jpg",
     link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiD87zRibnzAhXvBmMBHTY4DM0QmhN6BAhDEAI&url=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FHunter_%25C3%2597_Hunter&usg=AOvVaw3T0sTBH9dQQJniRHVPWDlG",
   },
   {
-    name: "Fullmetal Alchemist",
+    name: "Fullmetal Alchemist Brotherhood",
     description:
       "Fullmetal Alchemist, est un manga d'Hiromu Arakawa. Il a été prépublié entre 2001 et 2010 dans le magazine mensuel Shōnen Gangan de la société Square Enix et a été compilé en 27 volumes reliés. La version française est publiée par Kurokawa depuis 2005...",
-    img: "img/mangas/fullmetalalchemist.jpg",
+    img: "https://fr.web.img6.acsta.net/pictures/19/07/29/15/50/2364027.jpg",
     link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjwxdeTirnzAhVT8uAKHaB0D0MQmhN6BAg7EAI&url=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FFullmetal_Alchemist&usg=AOvVaw3zkIYNjHSncaMnemB-WIqH",
   },
   {
     name: "L'Attaque des Titans",
     description:
       "L’Attaque des Titans est un shōnen manga écrit et dessiné par Hajime Isayama. Il est prépublié entre septembre 2009 et avril 2021 dans le magazine Bessatsu Shōnen Magazine puis compilé en trente-quatre volumes reliés par l’éditeur Kōdansha...",
-    img: "img/mangas/lattaquedestitans.jpg",
+    img: "https://fr.web.img6.acsta.net/pictures/20/12/28/10/24/5603983.jpg",
     link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjmwozJirnzAhWTmhQKHROyDS0QmhN6BAg_EAI&url=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FL%2527Attaque_des_Titans&usg=AOvVaw2-X5Uu_7ImQJfHheCtCEpw",
   },
   {
     name: "Fate/stay night : Unlimited Blade Works",
     description:
       "Un groupe de sept mages est choisi pour devenir les maîtres de sept classes d'esprits héroïques, afin de combattre et de gagner le Saint Graal...",
-    img: "img/mangas/fatestaynight.jpg",
+    img: "https://lyricsfromanime.com/animes-info/fate-stay-night-unlimited-blade-works-tv/cover/fate-stay-night-unlimited-blade-works-tv-lyrics.jpg",
     link: "https://en.wikipedia.org/wiki/Fate/stay_night:_Unlimited_Blade_Works_(TV_series)",
   },
 ];
